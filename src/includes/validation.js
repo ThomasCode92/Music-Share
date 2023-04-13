4b476f7d43be092a8f5e6bc8d9133fef06ff5ace
ThomasCode92 <thomascode92@hotmail.com>
2023-04-13 16:51:54 +0200

    Defining Rules

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,11 @@
-import { Field as VeeField, Form as VeeForm } from 'vee-validate';
+import { Field as VeeField, Form as VeeForm, defineRule } from 'vee-validate';
+import { required } from '@vee-validate/rules';
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
+
+    defineRule('required', required);
   },
 };
