04d9942944c438c3f627f8e5f3646ca2d8b0d80b
ThomasCode92 <thomascode92@hotmail.com>
2023-04-17 08:02:52 +0200

    Applying Rules

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,10 +1,16 @@
-import { Field as VeeField, Form as VeeForm, defineRule } from 'vee-validate';
+import {
+  ErrorMessage,
+  Field as VeeField,
+  Form as VeeForm,
+  defineRule,
+} from 'vee-validate';
 import { required } from '@vee-validate/rules';
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
+    app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
   },
