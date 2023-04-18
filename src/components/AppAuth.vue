1c16f35674de810cb8095cc71a66784225063bf5
ThomasCode92 <thomascode92@hotmail.com>
2023-04-18 08:53:26 +0200

    Default Values

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -95,6 +95,7 @@
           <vee-form
             v-show="tab === 'register'"
             :validation-schema="schema"
+            :initial-values="userData"
             @submit="register"
           >
             <!-- Name -->
@@ -215,6 +216,9 @@ export default {
         confirm_password: 'confirmed:@password',
         country: 'required|not_one_of:Antarctica',
         tos: 'required',
+      },
+      userData: {
+        country: 'USA',
       },
     };
   },
