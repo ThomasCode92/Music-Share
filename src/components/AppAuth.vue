69c03d58bb3c2f1eed32dffb69936bf54011ebdd
ThomasCode92 <thomascode92@hotmail.com>
2023-04-18 08:40:53 +0200

    Validating the Form

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -92,7 +92,11 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -211,6 +215,11 @@ export default {
     ...mapWritableState(useModalStore, {
       modalVisibility: 'isOpen',
     }),
+  },
+  methods: {
+    register(values) {
+      console.log(values);
+    },
   },
 };
 </script>
