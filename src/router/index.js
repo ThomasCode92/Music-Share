c695c568499dbb0d84ab8bdfecfaba1fdb145ed2
ThomasCode92 <thomascode92@hotmail.com>
2023-06-03 17:37:53 +0200

    Tailwind Styles for Active Links

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import routes from './routes';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
