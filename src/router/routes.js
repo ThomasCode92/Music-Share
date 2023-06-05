16050c4d46825d718b645e7cfd2c32822a5e721b
ThomasCode92 <thomascode92@hotmail.com>
2023-06-05 18:55:19 +0200

    Setting up “Catch-All” and Redirect Routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,9 @@ import AppManage from '../views/AppManage.vue';
 
 const routes = [
   { path: '/', name: 'home', component: AppHome },
-  { path: '/manage', name: 'manage', component: AppManage },
+  { path: '/manage', redirect: { name: 'manage' } },
+  { path: '/manage-music', name: 'manage', component: AppManage },
+  { path: '/:catchAll(.*)*', redirect: { name: 'home' } },
 ];
 
 export default routes;
