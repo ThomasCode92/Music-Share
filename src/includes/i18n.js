90c0bebd5bb2d30af086c74162e650eeb7f941fa
ThomasCode92 <thomascode92@hotmail.com>
2023-08-01 20:53:55 +0200

    A First Translation

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,9 +1,10 @@
 import { createI18n } from 'vue-i18n';
 
 import en from '../locales/en.json';
+import nl from '../locales/nl.json';
 
 export default createI18n({
   locale: 'en',
   fallbackLocale: 'en',
-  messages: { en },
+  messages: { en, nl },
 });
