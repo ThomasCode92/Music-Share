dc660f56e5fcad4b3e34c54eee6d23fd42633ece
ThomasCode92 <thomascode92@hotmail.com>
2023-08-08 07:54:45 +0200

    Progress Bar

--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ import { registerSW } from 'virtual:pwa-register';
 import App from './App.vue';
 import './assets/base.css';
 import './assets/main.css';
+import 'nprogress/nprogress.css';
 
 import GlobalComponents from './includes/_globals';
 import VeeValidatePlugin from './includes/validation';
+import progressBar from './includes/progress-bar';
 import firebase from './includes/firebase';
 import i18n from './includes/i18n';
 import Icon from './directives/icon';
 import router from './router';
 
 registerSW({ immediate: true });
+
+progressBar(router);
 
 let vueApp;
 
