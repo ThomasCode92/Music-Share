271efb68077e1850e1ed06a9e1fc2a4e6f6ce1d5
ThomasCode92 <thomascode92@hotmail.com>
2023-08-08 13:05:03 +0200

    Deploying an App with Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,6 +24,5 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,png,jpg}'],
       },
     }),
-    visualizer({ open: true }),
   ],
 });
