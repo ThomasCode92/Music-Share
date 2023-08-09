ada815039e5e4614981bd86f34afebadb623ad1a
ThomasCode92 <thomascode92@hotmail.com>
2023-08-09 21:23:40 +0200

    Mounting with Vue Test Utils

---
+++ src/components/__tests__/about-page.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils';
+
+import AboutPage from '../../views/AboutPage.vue';
+
+describe('AboutPage.vue', () => {
+  test('should render the inner text', () => {
+    const wrapper = mount(AboutPage);
+  });
+});
