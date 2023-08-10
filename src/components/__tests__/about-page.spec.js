d5cf68e18d5b32e37c8eea027a5bb1fc611e40ee
ThomasCode92 <thomascode92@hotmail.com>
2023-08-10 07:43:58 +0200

    Testing the Inner Content

--- src/components/__tests__/about-page.spec.js
+++ src/components/__tests__/about-page.spec.js
@@ -4,6 +4,7 @@ import AboutPage from '../../views/AboutPage.vue';
 
 describe('AboutPage.vue', () => {
   test('should render the inner text', () => {
-    const wrapper = mount(AboutPage);
+    const wrapper = mount(AboutPage, { shallow: true });
+    expect(wrapper.text()).toContain('about');
   });
 });
