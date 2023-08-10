23eabe4665ca3ddd1b7331210b7af26df7b67bf1
ThomasCode92 <thomascode92@hotmail.com>
2023-08-10 08:03:32 +0200

    Stubbing Components

--- src/components/__tests__/song-item.spec.js
+++ src/components/__tests__/song-item.spec.js
@@ -1,11 +1,21 @@
-import { mount } from '@vue/test-utils';
+import { RouterLinkStub, mount } from '@vue/test-utils';
 
 import SongItem from '../SongItem.vue';
 
 describe('SongItem.vue', () => {
   test('should render the display name of the song', () => {
     const song = { display_name: 'test' };
-    const wrapper = mount(SongItem, { shallow: true, props: song });
-    expect(wrapper.text()).toContain(song.display_name);
+
+    const wrapper = mount(SongItem, {
+      shallow: true,
+      props: { song },
+      global: {
+        components: { 'router-link': RouterLinkStub },
+      },
+    });
+
+    const compositionAuthor = wrapper.find('.text-gray-500');
+
+    expect(compositionAuthor.text()).toBe(song.display_name);
   });
 });
